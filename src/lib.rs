4d59945c94a59c564202e1bbeb58a1849bfa74ee
Libing Chen <libing.chen@gmail.com>
2025-05-31 18:13:30 +0800

    app: lib mod

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+extern crate duckdb;
+extern crate duckdb_loadable_macros;
+extern crate libduckdb_sys;
+mod open_metrics;
+mod prometheus;
+
+use duckdb::{Connection, Result};
+use duckdb_loadable_macros::duckdb_entrypoint_c_api;
+use libduckdb_sys::{self as ffi};
+
+use crate::open_metrics::OpenMetricsVTab;
+use crate::prometheus::PrometheusVTab;
+use std::error::Error;
+
+#[duckdb_entrypoint_c_api()]
+pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
+    con.register_table_function::<PrometheusVTab>("prometheus")
+        .expect("Failed to register prometheus table function");
+
+    con.register_table_function::<OpenMetricsVTab>("openmetrics")
+        .expect("Failed to register openmetrics table function");
+
+    Ok(())
+}
